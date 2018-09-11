2e24fc53e7f7ccc08c5d5dc5fc26a303bbcce9f4
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-09-11 16:05:53 +1000

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# PATS
+PowerShell Acceptance Testing System
+
+## What's this?
+PATS [will...] be a test framework analgous to the much-loved [BATS](https://github.com/bats-core/bats-core) for Unix-like OSs.
+
+## Syntax?
+Still have a lot to learn about PowerShell, syntax will aim to be as close to BATS as possible but may need to be bent to requirements somewhat. There is DSL support in PowerShell which there are plans to leverage.
+
+## Support?
+No support yet due to lack of any code ;-). If your interested in sponsoring development please email sales@declarativesystems.com to start the conversation.
+
+## Helps?
+If you'd like to help on this project, that would be awesome. Tickets, PRs, general feedback, etc. all gratefully received.
---
+++ pats.ps1
@@ -0,0 +1,115 @@
+﻿Set-StrictMode -Version 2.0
+param (
+  [String] $TestCase = 'c:\vagrant\test.pats'
+)
+
+$testNameKey = "testName"
+$testPSKey   = "testPS"
+
+
+function ParseTestcase() {
+    param(
+        [string] $TestCase
+    )
+
+    # read the file as one long string
+    [String]$raw = Get-Content $TestCase -raw
+    $testRe = '@test "([^"]+)"\s*{\s*(.*)\s*}'
+
+    $matches = [regex]::Matches($raw, $testRe)
+    $tests = @()
+    foreach ($match in $matches) {
+        $testName = $match.captures.groups[1]
+        $testPS = $match.captures.groups[2]
+
+        $test = @{
+            $testNameKey=$testName;
+            $testPSKey = $testPS
+        }
+        $tests += $test
+    }
+    return $tests
+}
+    
+  #  if ($matches.success) {
+  #      write-host "its seuccess"
+  #      $i = 1
+#
+ #       foreach ($match in [regex]::Matches($raw, $testRe, $i)) {
+ #           write-host "lOL"
+#
+ #           write-host $matches.captures.groups[1]
+  #          $i += 1
+  #      }
+   # }
+
+
+   #write-host $matches.captures.groups.Count
+
+
+#    foreach ($line in $raw) {
+#        ($url.ToCharArray() | Where-Object {$_ -eq ''} | Measure-Object).Count
+
+
+#        if ($insideTest -and $insidePS) {
+#            # scan for the `}` indicating end of and powershell
+#            if ($line -match $endTestRe
+#        if ($line -match $testNameRe) {
+#            $insideTest = $true
+#            
+#            $testName = [regex]::Match($line, $testNameRe).captures.groups[1]
+#            $tests[$testName] = ""
+#        } else {
+#            # append to current testcase
+#            $tests[$testName] += $line
+#        }
+#    }
+
+
+  #  write-host "parsed tests OK"
+  #  foreach ($test in $tests.Keys) {
+  #      Write-Host "${test}: $($tests.Item($test))"
+  #  }
+
+function RunTest() {
+    param([string] $PS)
+
+
+    return 0
+}
+
+
+function RunTests() {
+    param(
+        [array] $Tests
+    )
+
+    $failedTests = 0
+    foreach ($test in $Tests) {
+        $status = RunTest $test[$testPSKey]
+        if ($status -eq 0) {
+            $emoji = "✓"
+        } else {
+            $emoji = "✗"
+            $failedTests += 1
+        }
+        
+        write-host " $($emoji) $($test[$testNameKey])"
+    }
+
+    write-host "$($tests.Count) test, $($failedTests) failure"
+}
+
+
+
+if ($TestCase -eq "") {
+    write-error "Must specify Testcase File"
+} else {
+    if (Test-Path $TestCase) {
+        write-host "processing tests in $($TestCase)"
+        $tests = ParseTestcase $TestCase
+        RunTests $tests
+    } else {
+        Write-Error "Missing testcase file: $($TestCase)"
+    }
+}
